1a1d107912292368dfc737d63a0dba361efce5e8
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 12:35:09 +0000

    "Added error handling to findPeopleByName function"

--- myApp.js
+++ myApp.js
@@ -40,7 +40,14 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) {
+      console.log(err);
+      done(err, null);
+    }
+    done(null, data);
+  }
+  );
 };
 
 

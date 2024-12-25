3d119f5dcd6460281418cfa95a6a76fb8edf264c
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 12:36:51 +0000

    `Added error handling to findPersonById function`

--- myApp.js
+++ myApp.js
@@ -63,7 +63,14 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      console.log(err);
+      done(err, null);
+    }
+    done(null, data);
+  }
+  );
 };
 
 const findEditThenSave = (personId, done) => {
